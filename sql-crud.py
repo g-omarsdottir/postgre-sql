36aaafc51906b08d00e509b5718c2a995056e234
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-04-22 15:08:09 +0000

    Update and delete records

--- sql-crud.py
+++ sql-crud.py
@@ -96,10 +96,50 @@ one_more = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(one_more)
+# session.add(one_more)
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=8).first()
+# programmer.famous_for = "Getting the hang of it"
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#    if person.gender == "F":
+#        person.gender = "Female"
+#    elif person.gender == "M":
+#        person.gender = "Male"
+#   else:
+#        print("Gender not defined")
+#    session.commit()
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+# if programmer is not None:
+#    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#    if confirmation.lower() == "y":
+#        session.delete(programmer)
+#        session.commit()
+#        print("Programmer has been deleted")
+#    else:
+#        print("Programmer not deleted")
+# else:
+#    print("No records found")
+
+### DANGER ZONE ### HOUSTON WE HAVE A PROBLEM AREA ### DELETES ALL DATA ###
+# delete multiple/all records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+
 
 # commit our session to the database
-session.commit()
+# session.commit()
 
 
 # query the database to find all Programmers
